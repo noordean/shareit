7948a39ba5b79a1e73a76421029c39fe897e71b4
noordean <ebroyeem90@gmail.com>
2018-04-23 17:28:17 +0100

    chore(sequelize): setup sequelize with mysql
    - add sequelize, sequelize-cli and mysql2 packages
    - create migration for Item
    
    [Finishes #156304277]

---
+++ config/config.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    dialect: 'mysql'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: 'localhost',
+    dialect: 'mysql'
+  },
+  production: {
+
+  }
+};
---
+++ migrations/20180423143109-create-item.js
@@ -0,0 +1,42 @@
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('Items', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      category: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      downloadLink: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      keywords: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface /* , Sequelize */) => {
+    queryInterface.dropTable('Items');
+  }
+};
